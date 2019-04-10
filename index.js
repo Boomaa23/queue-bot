c4998a22a627070cae7943a6183d5f956c0b63f4
Boomaa23 <cmo93003@yahoo.com>
2019-04-09 22:32:07 -0700

    Message input testing

--- index.js
+++ index.js
@@ -6,28 +6,35 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
+client.on('message', message => {
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const cmd = args.shift().toLowerCase();
+  console.log("message");
   if(message.content.indexOf(config.prefix) === 0) {
+    console.log("outerif");
     if(command === "queue") {
-       message.channel.send('queue');
+      message.channel.send('queue');
+      console.log("queue");
     }
 
     if(command === "ping") {
       message.channel.send('ping');
+      console.log("ping");
     }
     
     if(command === "add") {
       message.channel.send('add');
+      console.log("add");
     }
     
     if(command === "remove") {
       message.channel.send('remove');
+      console.log("remove");
     }
     
     if(command === "clear") {
       message.channel.send('clear');
+      console.log("clear");
     }
   }
 });
